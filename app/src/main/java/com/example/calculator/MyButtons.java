d1edee8a63cca738aaadc460d15ffcc2cc8e222c
ImponedSoftware <59576301+ImponedSoftware@users.noreply.github.com>
2020-11-30 21:48:10 -0800

    Double operations along with functional negative operations. I added the negative sign button and fixed bugs with clicking operators and the screen not clearing

--- app/src/main/java/com/example/calculator/MyButtons.java
+++ app/src/main/java/com/example/calculator/MyButtons.java
@@ -5,8 +5,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.example.calculator.R;
-
 
 public class MyButtons extends ComputeFunctions implements View.OnClickListener {
     private Activity activity;
@@ -54,6 +52,8 @@ public class MyButtons extends ComputeFunctions implements View.OnClickListener 
         buttonEquals.setOnClickListener(this);
         Button buttonDec = (Button) activity.findViewById(R.id.buttonDec);
         buttonDec.setOnClickListener(this);
+        Button buttonNegative = (Button) activity.findViewById(R.id.buttonNegative);
+        buttonNegative.setOnClickListener(this);
         Button buttonClear = (Button) activity.findViewById(R.id.buttonClear);
         buttonClear.setOnClickListener(this);
     }
@@ -64,101 +64,143 @@ public class MyButtons extends ComputeFunctions implements View.OnClickListener 
             case R.id.button0:
                 if(previousResultActivated) break;
                 holdTempResult += "0";
-                textView.setText(holdTempResult);
+                holdTempCopy += "0";
+                textView.setText(holdTempCopy);
                 break;
             case R.id.button1:
                 if(previousResultActivated) break;
                 holdTempResult += "1";
-                textView.setText(holdTempResult);
+                holdTempCopy += "1";
+                textView.setText(holdTempCopy);
                 break;
             case R.id.button2:
                 if(previousResultActivated) break;
                 holdTempResult += "2";
-                textView.setText(holdTempResult);
+                holdTempCopy += "2";
+                textView.setText(holdTempCopy);
                 break;
             case R.id.button3:
-//                if(previousResultActivated)
-//                {
-//                    holdTempResult = "";
-//                    numbers.clear();
-//                }
                 if(previousResultActivated) break;
                 holdTempResult += "3";
-                textView.setText(holdTempResult);
+                holdTempCopy += "3";
+                textView.setText(holdTempCopy);
                 break;
             case R.id.button4:
                 if(previousResultActivated) break;
                 holdTempResult += "4";
-                textView.setText(holdTempResult);
+                holdTempCopy += "4";
+                textView.setText(holdTempCopy);
                 break;
             case R.id.button5:
                 if(previousResultActivated) break;
                 holdTempResult += "5";
-                textView.setText(holdTempResult);
+                holdTempCopy += "5";
+                textView.setText(holdTempCopy);
                 break;
             case R.id.button6:
                 if(previousResultActivated) break;
                 holdTempResult += "6";
-                textView.setText(holdTempResult);
+                holdTempCopy += "6";
+                textView.setText(holdTempCopy);
                 break;
             case R.id.button7:
                 if(previousResultActivated) break;
                 holdTempResult += "7";
-                textView.setText(holdTempResult);
+                holdTempCopy += "7";
+                textView.setText(holdTempCopy);
                 break;
             case R.id.button8:
                 if(previousResultActivated) break;
                 holdTempResult += "8";
-                textView.setText(holdTempResult);
+                holdTempCopy += "8";
+                textView.setText(holdTempCopy);
                 break;
             case R.id.button9:
                 if(previousResultActivated) break;
                 holdTempResult += "9";
-                textView.setText(holdTempResult);
+                holdTempCopy += "9";
+                textView.setText(holdTempCopy);
                 break;
             case R.id.buttonAdd:
-                if(previousResultActivated) numbers.remove(0); // del
+                if(previousResultActivated) {
+                    holdTempCopy = "";
+                    numbers.remove(0); // del
+                }
+
                 previousResultActivated = false;
                 holdTempResult += ADD;
-                textView.setText(holdTempResult);
+                holdTempCopy += ADD;
+                textView.setText(holdTempCopy);
                 break;
             case R.id.buttonMultiply:
-                if(previousResultActivated) numbers.remove(0); // del
+                if(previousResultActivated){
+                    holdTempCopy = "";
+                    numbers.remove(0); // del
+                }
+
                 previousResultActivated = false;
-                holdTempResult += MUTIPLY;
-                textView.setText(holdTempResult);
+                holdTempResult += MULTIPLY;
+                holdTempCopy += MULTIPLY;
+                textView.setText(holdTempCopy);
                 break;
             case R.id.buttonSubtract:
-                if(previousResultActivated) numbers.remove(0); // del
+                if(previousResultActivated)
+                {
+                    holdTempCopy = "";
+                    numbers.remove(0); // del
+                }
+
                 previousResultActivated = false;
                 holdTempResult += SUBTRACT;
-                textView.setText(holdTempResult);
+                holdTempCopy += SUBTRACT;
+                textView.setText(holdTempCopy);
                 break;
             case R.id.buttonDivide:
-                if(previousResultActivated) numbers.remove(0); // del
+                if(previousResultActivated)
+                {
+                    holdTempCopy = "";
+                    numbers.remove(0); // del
+                }
+
                 previousResultActivated = false;
                 holdTempResult += DIVIDE;
-                textView.setText(holdTempResult);
+                holdTempCopy += DIVIDE;
+                textView.setText(holdTempCopy);
                 break;
             case R.id.buttonDec:
                 holdTempResult += ".";
-                textView.setText(holdTempResult);
+                System.out.println("Decimal pressed");
+                holdTempCopy += ".";
+                textView.setText(holdTempCopy);
+                break;
+            case R.id.buttonNegative:
+                holdTempCopy += "-";
+                holdTempResult += "_";
+                System.out.println("Negative pressed");
+                textView.setText(holdTempCopy);
                 break;
             case R.id.buttonEquals:
-//                if(opFixed){
-//                    textView.setText("");
-//                    opFixed = false;
-//                    break;
-//                }
-                holdTempResult = Integer.toString(computeResult());
+                double answer = computeResult();
+                holdTempResult = Double.toString(answer);
                 System.out.printf("Your holdTempResult when equal is pressed is: %s\n", holdTempResult);
+
+                // to account for a negative number not having the underscore prefix
+                // Ex: -10 will throw error because program recognizes "-" as an operator
+                //   : This will replace "-" with "_", so the program can distinguish operator versus sign
+
                 previousResultActivated = true;
+                previousNumberIsNegative = answer < 0 ? true : false;
+
+                System.out.printf("Your holdTempResult when after operator change: %s\n", holdTempResult);
+
                 textView.setText(holdTempResult);
                 break;
             case R.id.buttonClear:
                 if(previousResultActivated && numbers.size() != 0) numbers.remove(0);
                 holdTempResult = "";
+                holdTempCopy = "";
                 previousResultActivated = false;
+                previousNumberIsNegative = false;
                 textView.setText(holdTempResult);
                 break;
         }
